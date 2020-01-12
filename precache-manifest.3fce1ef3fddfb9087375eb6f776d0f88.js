cf19ce0c96c975c9e3b431f0472ad15bcffeac4e
Jared Harbison <jared.harbison@gmail.com>
2020-01-12 16:43:03 -0500

    Updates

---
+++ precache-manifest.3fce1ef3fddfb9087375eb6f776d0f88.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6646ffeef6464702a4de0c66f139efb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4da76533a76a24f8717",
+    "url": "/static/css/main.d06c2727.chunk.css"
+  },
+  {
+    "revision": "7c0d074a957a41d32350",
+    "url": "/static/js/2.f5a4d8ed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f5a4d8ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4da76533a76a24f8717",
+    "url": "/static/js/main.4ea299a9.chunk.js"
+  },
+  {
+    "revision": "6c75d82ec72c241128da",
+    "url": "/static/js/runtime-main.570caf92.js"
+  },
+  {
+    "revision": "5a41dcc3f442b0de24042163c38d13c5",
+    "url": "/static/media/rom-and-moon.5a41dcc3.png"
+  },
+  {
+    "revision": "9a14d6b98a9733dd77e131fc39b5bbbc",
+    "url": "/static/media/rom-cutout-downside.9a14d6b9.png"
+  },
+  {
+    "revision": "80c29627081ad27fd6ca0e0be148668d",
+    "url": "/static/media/rom-cutout-upside.80c29627.png"
+  }
+]);
--- src/fontawesome.js
+++
@@ -1,9 +0,0 @@
-// import the library
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-// import your icons
-import { faEnvelope,  } from '@fortawesome/free-solid-svg-icons'
-
-library.add(
-    fab,
-    faEnvelope)
