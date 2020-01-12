8386ee4711a7cb1b7fa778fdff9c8eb739669d33
Jared Harbison <jared.harbison@gmail.com>
2020-01-12 16:38:58 -0500

    added fontawesome library

---
+++ src/fontawesome.js
@@ -0,0 +1,9 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+// import your icons
+import { faEnvelope,  } from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+    fab,
+    faEnvelope)
