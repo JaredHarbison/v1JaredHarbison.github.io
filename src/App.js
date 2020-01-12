0028469def08747df278e34963b27c0c39f7c755
Jared Harbison <jared.harbison@gmail.com>
2020-01-12 16:37:40 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './fontawesome.js';
+
+import Background from './Components/Background/Background.jsx';
+import Footer from './Components/Footer/Footer.jsx';
+import LogoBanner from './Components/LogoBanner/LogoBanner.jsx';
+import TechStack from './Components/TechStack/TechStack.jsx';
+import Portfolio from './Components/Portfolio/Portfolio.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Background/>
+      <LogoBanner/>
+      <TechStack/>
+      <Portfolio/>
+      <Footer/>
     </div>
   );
 }
